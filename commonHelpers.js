09a576f0121f3daebef81c591397dfa9e5c1f0ef
NazarHook <145909670+NazarHook@users.noreply.github.com>
2024-02-20 22:39:17 +0000

    Deploying to gh-pages from @ NazarHook/goit-js-hw-09@56d3ddda2deec8bb768bef2cb56edad6de2c1c6a 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";console.log("gallery");
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{S as o}from"./assets/vendor-870f0eb5.js";$(".gallery a").simpleLightbox({});console.log(o);
 //# sourceMappingURL=commonHelpers.js.map
