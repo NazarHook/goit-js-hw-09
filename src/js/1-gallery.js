56d3ddda2deec8bb768bef2cb56edad6de2c1c6a
NazarHook <145909670+NazarHook@users.noreply.github.com>
2024-02-21 00:38:57 +0200

    ок

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,1 +1,4 @@
-console.log("gallery")
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+const lightbox = $('.gallery a').simpleLightbox({ /* options */ });
+console.log(SimpleLightbox);
